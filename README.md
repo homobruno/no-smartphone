b46d45795c57eab18258a9a4ad3ac9d8937fa271
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-16 17:02:30 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ohana-hotels.github.io
-Ohana Hotels website
+# Ohana Hotels
+
+- forked from https://github.com/garrettboatman/Blank-Theme-Jekyll
+- http://www.online-convert.com/
+- http://petrosh.github.io/Blank-Theme-Jekyll/
+- http://demosthenes.info/samples/polina.html
---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+
+	// $('body').videoBG({
+	// 	position:"fixed",
+	// 	zIndex:0,
+	// 	mp4:'assets/video/goacorto.mp4',
+	// 	ogv:'assets/video/goacorto.ogv',
+	// 	webm:'assets/video/goacorto.webm',
+	// 	poster:'assets/video/goa.jpg',
+	// 	opacity:1,
+	// 	fullscreen:true,
+	// });
+
+})
